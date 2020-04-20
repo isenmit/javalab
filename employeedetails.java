141f7c7569a29134f632f58d690fbd433221a7c4
harshitojha <37072234+harshitojha@users.noreply.github.com>
2020-04-20 23:33:37 +0530

    Add files via upload

---
+++ employeedetails.java
@@ -0,0 +1,43 @@
+
+import java.util.Date;
+import java.util.Stack;
+public class employeedetails
+{
+	public static void main(String args[])
+	{
+		System.out.println("Enter Valid Employee ID : \n");				//enter an id from the specified list
+		int[] EmpId={1001,1002,1003,1004,1005,1006,1007};				//valid array of IDs
+		String[] EmpName={"Abc","Opqr","Ghi","Wxyz","Jklmn","Stuv","Def"};		//array of names according to the IDs respectively
+		String[] JoinDate={"01/04/2009","23/08/2012","12/11/2008","29/01/2013","16/07/2005","01/01/2000","12/06/2006"};	//array of joining dates according to the IDs respectively.
+		char[] DesigCode={'e','c','k','r','m','e','c'};					//array of designation codes according to the IDs respectively.
+		String[] Department={"R&D","PM","Acct","Front Desk","Engg","Manufacturing","PM"};	//array of department of the employees according the IDs respectively
+		double[] Basic={20000,30000,10000,12000,50000,23000,29000};			//array of basic salaries of employees.
+		double[] HRA={8000,12000,8000,6000,20000,9000,12000};				//array of hra of employees
+		double[] IT={3000,9000,1000,2000,20000,4400,10000};					//array of income tax of employees
+		char[] DesignationCode={'e','c','k','r','m'};
+		String[] Designation={"Engineer","Consultant","Clerk","Receptionist","Manager"};	//array of designations
+		double[] DA={20000,32000,12000,15000,40000};						//array of da of employees
+		int flag=0;
+		int id=Integer.parseInt(args[0]);
+
+		for(int i=0;i<EmpId.length;i++)
+		{
+			if(EmpId[i]==id)
+			{
+				flag=1;
+				System.out.println("Emp Id.	 Emp Name	Department	Designation	   DA");		//printing employee details of specified employee id
+				System.out.print(EmpId[i]+" 	  "+EmpName[i]+" 		"+Department[i]);
+				for(int j=0;j<DesignationCode.length;j++)
+				{
+					if(DesigCode[i]==DesignationCode[j])
+					{	System.out.print(" 		"+Designation[j]+" 	");
+						double sum=Basic[i]+HRA[i]+DA[j]-IT[i];					//calculating sum
+						System.out.print(sum);
+					}
+				}
+			}
+		}
+		if(flag==0)
+			System.out.println("There is no employee with EmpId : " +id);		//prints this when an invalid employee ID is returned
+	}
+}
