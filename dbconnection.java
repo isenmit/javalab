8b5658f84b7eb3c72698cc4416972515629f3c5d
Abhinav2807 <37084476+Abhinav2807@users.noreply.github.com>
2020-04-21 02:20:28 +0530

    Add files via upload
    Commit by Abhinav

---
+++ dbconnection.java
@@ -0,0 +1,31 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbconnection
+{
+	public Connection getDBconnection()
+	{
+		Connection conn=null;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");			//specifies the driver
+			System.out.println("Registered successfully");
+		}
+		catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		try
+		{
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/java","Abhinav","@abhi28");	//database name, user name,password
+			System.out.println("Connection successfull\n");	//displayed if connection happens successfully
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
